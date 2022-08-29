d64676a352c27a6548258ff4213f766a01d98169
G-vans <jevans.otieno@student.moringaschool.com>
2022-08-29 05:34:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+function reverseString(word) {
+  return word.split("").reverse().join("");
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  const reversedWord = reverseString(word);
+  return word === reversedWord;
 }
 
 /* 
@@ -13,13 +18,30 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log("Expecting: true")
+  console.log("=>", isPalindrome("abba"))
+
+  console.log("")
+
+  console.log("Expecting: true")
+  console.log("=>", isPalindrome("racecar"))
+
+  console.log("")
+
+  console.log("Expecting: true")
+  console.log("=>",isPalindrome("a"))
+
+  console.log("")
+
+
   console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+  console.log("=>", isPalindrome("robot"));
 
   console.log("");
 
   console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  console.log("=>", isPalindrome("ab"));
 }
+
 
 module.exports = isPalindrome;
